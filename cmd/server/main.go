9e631976623519a6e1a2abe5145fc4a0454e13a8
Kirill <kenueyy@yandex.ru>
2026-03-05 13:43:05 +0700

    create main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/KenueYy/wallet-api/internal/config"
+	"github.com/KenueYy/wallet-api/internal/db"
+	"github.com/KenueYy/wallet-api/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := config.Load()
+
+	if err := db.Init(cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+	handlers.RegisterRoutes(r)
+
+	log.Printf("Server port :%d", cfg.Port)
+	if err := r.Run(":" + strconv.Itoa(cfg.Port)); err != nil {
+		log.Fatal(err)
+	}
+}
